7ce3a5fea4fc44e42b1448585930f6956d1c2c39
Bogdan Stefan <sbogdanm26@hotmail.com>
2022-03-19 00:05:49 +0100

    Task - 007: Done (changes regarding the money attribute and creation date)

--- src/main/java/acme/entities/patronage/Patronage.java
+++ src/main/java/acme/entities/patronage/Patronage.java
@@ -6,14 +6,15 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Positive;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
+import acme.framework.datatypes.Money;
 import acme.framework.entities.AbstractEntity;
 import lombok.Getter;
 import lombok.Setter;
@@ -35,20 +36,24 @@ public class Patronage extends AbstractEntity {
 	protected String code;
 
 	@NotBlank
-	@Length(max = 255)
+	@Length(min = 0, max = 255)
 	protected String legalStuff;
 
-	@Positive
 	@NotNull
-	protected Double budget;  //NO ES MEJOR FLOAT
-
-	@Temporal(TemporalType.TIMESTAMP)
-	@NotNull
-	protected Date iniDate;  //no es mejor startDate
+	@Valid
+	protected Money budget; 
 	
 	@Temporal(TemporalType.TIMESTAMP)
 	@NotNull
-	protected Date finishDate;
+	protected Date creationDate;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	protected Date startDate;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	protected Date endDate;
 	
 	@URL
 	protected String link;
