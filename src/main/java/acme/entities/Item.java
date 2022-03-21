f8e8f8afdae8c45324da71a3419984eb1e899a46
Jose Manuel <joslobtro@alum.us.es>
2022-03-21 21:58:20 +0100

    Task-005 Item
    Relaciones de Item implementadas

--- src/main/java/acme/entities/Item.java
+++ src/main/java/acme/entities/Item.java
@@ -14,9 +14,12 @@ package acme.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import acme.framework.datatypes.Money;
@@ -25,6 +28,7 @@ import org.hibernate.validator.constraints.URL;
 
 
 import acme.framework.entities.AbstractEntity;
+import acme.roles.Inventor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -65,6 +69,12 @@ public class Item extends AbstractEntity {
 	
 	@URL
 	protected String link;
+	
+	
+	@NotNull
+	@Valid
+	@ManyToOne(optional=false)
+	protected Inventor inventor;
 
 	// Derived attributes -----------------------------------------------------
 
