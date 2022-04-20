b7b1f6826ced7f1f2bf11d9aae2599956d5957ea
luicersan <luicersan@alum.us.es>
2022-04-20 16:57:00 +0200

    Task 001: done.

---
+++ src/main/java/acme/features/any/chirp/ChirpRepository.java
@@ -0,0 +1,36 @@
+/*
+ * AnyDutyRepository.java
+ *
+ * Copyright (C) 2012-2022 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.features.any.chirp;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.entities.Chirp;
+import acme.framework.repositories.AbstractRepository;
+
+@Repository
+public interface ChirpRepository extends AbstractRepository {
+
+	@Query("select c from Chirp c where c.id = :id")
+	Chirp findOneChirpById(int id);
+	
+	@Query("select c from Chirp c")
+	List<Chirp> findAllChirps();
+	
+	//Falta el método para obtener Ids de menos de un mes
+//	@Query("select c from Chirp c where c.creation_moment < actualDate")
+//	Collection<Chirp> findRecentChirps(Date currentDate);
+
+}
