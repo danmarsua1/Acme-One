9238704f5518bfd13f072da55f88f8324e1569e5
luicersan <luicersan@alum.us.es>
2022-05-17 18:57:11 +0200

    Task-002: test done

--- src/test/java/acme/testing/any/chirp/ChirpCreateTest.java
+++ src/test/java/acme/testing/any/chirp/ChirpCreateTest.java
@@ -27,7 +27,7 @@ public class ChirpCreateTest extends TestHarness {
 	@ParameterizedTest
 	@CsvFileSource(resources = "/any/chirp/list-all.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
-	public void positiveTest(final int recordIndex,final String title, final String author, final String creationMoment, final String body, final String email) {
+	public void positiveTest(final int recordIndex, final String title, final String author, final String creationMoment, final String body, final String email) {
 		super.signIn("administrator", "administrator");
 
 		super.clickOnMenu("Any", "Chirp");
@@ -59,39 +59,26 @@ public class ChirpCreateTest extends TestHarness {
 	}
 
 //	@ParameterizedTest
-//	@CsvFileSource(resources = "/employer/duty/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
-//	@Order(20)
-//	public void negativeTest(final int jobRecordIndex, final int dutyRecordIndex, final String title, final String description, final String workLoad, final String moreInfo) {
-//		super.signIn("employer1", "employer1");
-//
-//		super.clickOnMenu("Employer", "List my jobs");
-//		super.checkListingExists();
-//		super.sortListing(0, "asc");
-//
-//		super.clickOnListingRecord(jobRecordIndex);
-//		super.clickOnButton("Duties");
-//
-//		super.clickOnButton("Create");
-//		super.fillInputBoxIn("title", title);
-//		super.fillInputBoxIn("description", description);
-//		super.fillInputBoxIn("workLoad", workLoad);
-//		super.fillInputBoxIn("moreInfo", moreInfo);
-//		super.clickOnSubmit("Create");
-//		super.checkErrorsExist();
-//
-//		super.signOut();
-//	}
-//
-//	@Test
-//	@Order(30)
-//	public void hackingTest() {
-//		// HINT: the framework doesn't currently provide enough support to hack
-//		// HINT+ this feature, so the remaining hacking tests must be performed manually.
-//		// HINT+ a) create a duty for a job as a principal without the "Employer" role;
-//		// HINT+ b) create a duty for a published job created by the principal;
-//		// HINT+ c) create a duty for an unpublished job that wasn't created by the principal.
-//	}
-//
-//	// Ancillary methods ------------------------------------------------------
+	@CsvFileSource(resources = "/any/chirp/list-all.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(20)
+	public void negativeTest(final int recordIndex,  final String title, final String author, final String creationMoment, final String body, final String email) {
+		super.signIn("administrator", "administrator");
+
+		super.clickOnMenu("Employer", "List my jobs");
+		super.checkListingExists();
+		super.sortListing(1, "asc");
+
+		super.clickOnButton("Create chirp");
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("author", author);
+		super.fillInputBoxIn("body", body);
+		super.fillInputBoxIn("email", email);
+//		super.fillInputBoxIn("confirmation", "true");
+		super.clickOnSubmit("Create chirp");
+
+		super.checkErrorsExist();
+
+		super.signOut();
+	}
 
 }
