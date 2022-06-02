92bba36d95c50400785c79653b88d1051d42227e
luicersan <luicersan@alum.us.es>
2022-06-02 19:37:43 +0200

    Task-testing: task007 test fixed

--- src/test/java/acme/testing/inventor/patronageReport/InventorPatronageReportCreateTest.java
+++ src/test/java/acme/testing/inventor/patronageReport/InventorPatronageReportCreateTest.java
@@ -1,6 +1,7 @@
 package acme.testing.inventor.patronageReport;
 
 import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
@@ -11,72 +12,49 @@ public class InventorPatronageReportCreateTest extends TestHarness {
 	@ParameterizedTest
 	@CsvFileSource(resources = "/inventor/patronage-report/create-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
-	public void positiveTest(final int recordIndex, final String sequenceNumber, final String creationMoment, final String memorandum, final String link) {
+	public void positiveTest(final int recordIndex, final String creationMoment, final String memorandum, final String link) {
 	    super.signIn("inventor2", "inventor2");
 
 	    super.clickOnMenu("Inventor", "Patronages");
 		super.checkListingExists();
-		super.sortListing(2, "asc");
+		super.sortListing(0, "desc");
+		
+		super.clickOnListingRecord(recordIndex);
 		
 		super.clickOnButton("Create Patronage Report");
-		super.clickOnButton("Confirm");
+		super.clickOnSubmit("Confirm");
 		super.checkFormExists();
-		super.fillInputBoxIn("sequenceNumber", sequenceNumber);
 		super.fillInputBoxIn("memorandum", memorandum);
 		super.fillInputBoxIn("link", link);
 		super.clickOnSubmit("Create");
 		
 		super.clickOnMenu("Inventor", "Patronages");
 		super.checkListingExists();
-		super.sortListing(2, "asc");
+		super.sortListing(0, "desc");
+		super.clickOnListingRecord(recordIndex);
 		super.clickOnButton("Show Patronage Report");
 		
 		super.checkFormExists();
-		super.checkInputBoxHasValue("sequenceNumber", sequenceNumber);
-		super.checkInputBoxHasValue("creationMoment", creationMoment);
 		super.checkInputBoxHasValue("memorandum", memorandum);
 		super.checkInputBoxHasValue("link", link);
 		super.signOut();
 	}
 	
-//	@ParameterizedTest
-//	@CsvFileSource(resources = "/inventor/patronage-report/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
-//	@Order(20)
-//	public void negativeTest (final int recordIndex, final String sequenceNumber, final String creationMoment, final String memorandum, final String link) {
-//		super.signIn("inventor2", "inventor2");
-//
-//	    super.clickOnMenu("Inventor", "Patronages");
-//		super.checkListingExists();
-//		super.sortListing(2, "asc");
-//		
-//		super.clickOnButton("Create Patronage Report");
-//		super.clickOnButton("Confirm");
-//		super.checkFormExists();
-//		super.fillInputBoxIn("sequenceNumber", sequenceNumber);
-//		super.fillInputBoxIn("memorandum", memorandum);
-//		super.fillInputBoxIn("link", link);
-//		super.clickOnSubmit("Create");
-//		
-//		super.checkErrorsExist();
-//		
-//		super.signOut();
-//	}
-//	
-//	@Test
-//	@Order(20)
-//	public void hackingTest() {
-//		super.checkNotLinkExists("Account");
-//		super.navigate("/inventor/patronage-report/create");
-//		super.checkPanicExists();
-//
-//		super.signIn("administrator", "administrator");
-//		super.navigate("/inventor/patronage-report/create");
-//		super.checkPanicExists();
-//		super.signOut();
-//
-//		super.signIn("patron1", "patron1");
-//		super.navigate("/inventor/patronage-report/create");
-//		super.checkPanicExists();
-//		super.signOut();
-//	}
+	@Test
+	@Order(20)
+	public void hackingTest() {
+		super.checkNotLinkExists("Account");
+		super.navigate("/inventor/patronage-report/create");
+		super.checkPanicExists();
+
+		super.signIn("administrator", "administrator");
+		super.navigate("/inventor/patronage-report/create");
+		super.checkPanicExists();
+		super.signOut();
+
+		super.signIn("patron1", "patron1");
+		super.navigate("/inventor/patronage-report/create");
+		super.checkPanicExists();
+		super.signOut();
+	}
 }
